62db3e5c8477f1b839c9527c36f6749f70f59436
agmonr <agmonr@gmail.com>
2015-05-25 21:45:30 +0300

    adding log file

--- frame.py
+++ frame.py
@@ -13,12 +13,19 @@ class frame:
     			for name in files:
 				if types.count(name[-3:])>0:
         				self.List.append( os.path.join(path, name) )
-		self.List.sort()
+		self.write_log("------------")
+		self.write_log("* Starting *")
 		self.main()
 
+	def write_log(self,Text):
+		f=open('frame.log','a')
+		f.write(time.strftime("%H:%M ")+Text+"\n")
+		f.close()
 
-	def read_img(self,FileName):
-		self.img=cv2.imread(FileName)
+
+	def read_img(self):
+		self.img=cv2.imread(self.FileName)
+		self.write_log(self.FileName+" "+str(self.img.shape))	
 		if GrayScale: 
 			self.img=cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
 		os.system('export DISPLAY=:0; /usr/bin/xset dpms force off') #making sure power save stays off
@@ -46,36 +53,39 @@ class frame:
 		if hours_on[self.Day].count(self.Hour)>0:
 			Status=subprocess.Popen("/usr/bin/tvservice -s", shell=True, stdout=subprocess.PIPE).stdout.read()
 			if Status.find('progressive') == -1:
-				print "Screen off - turning on"
+				self.write_log("Screen off - turning on")
 				os.system('/usr/bin/tvservice -p')
-				print "screen on"
 		else:
 			os.system('/usr/bin/tvservice -o')
+			self.write_log("Turning screen off")
 			while( time.strftime("%H") == self.Hour):
-				print time.strftime("%H")
-				print "sleeping"+str(self.Day)+" "+str(self.Hour)
-				time.sleep(60)
+				time.sleep(600)
 
+	def main1(self):
+		self.Hour=time.strftime("%H")
+		self.Day=datetime.datetime.today().weekday()
+		self.check_on()
+		self.read_img()
+		self.add_hour()
+		self.show()
+	
 
 	def main(self):
 		count=0
 		f=randint(0,len(self.List)-serias)
 		while 1:
-			FileName=self.List[f]
+			self.FileName=self.List[f]
 			try:
-				self.Hour=time.strftime("%H")
-				self.Day=datetime.datetime.today().weekday()
-				self.check_on()
-				self.read_img(FileName)
-				self.add_hour()
-				self.show()
-				count+=1
-				f+=1
-				if count>=serias:
-					count=0
-					f=randint(0,len(self.List)-serias)
+				self.main1()
 			except:
-				print "Problem with img"+FileName
+				print "Problem with img"+self.FileName
+				self.write_log( "Problem with img "+self.FileName )
+
+			count+=1
+			f+=1
+			if count>=serias:
+				count=0
+				f=randint(0,len(self.List)-serias)
 				
 
 Frame01 = frame()
