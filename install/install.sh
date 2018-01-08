18520e1accba7365149f28e3f6b3a0ac90e5c81a
Ram-on Agmon <ram-on.agmon@klarna.com>
2018-01-08 16:26:38 +0200

    bug fix refactoring

--- install/install.sh
+++ install/install.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+
+DEST='/opt/frame/'
+
 [[ "$USER" != "root" ]] && echo "This script should be run under root" && exit 2
 mkdir -p /opt/frame/
 if [ -d "/root/project" ]; then # we are in a vagrant box
-	cp -rfv /root/project/* /opt/frame/
+	cp -rfv /root/project/* "${DEST}"
 else
-	cp -rfv ../* /opt/frame/ # we are probably running from git clone
+	cp -rfv ../* "${DEST}" # we are probably running from git clone
 fi
 apt-get -y update
 apt-get -y upgrade
-apt-get install -y curl wget screen python-opencv xserver-xorg vim python-pip python-requests python-httplib2 mysql-server 
+apt-get install -y curl\
+  wget\
+  vim\
+  screen\
+  python-opencv\
+  python-pip\
+  python-requests\
+  python-httplib2\
+  xserver-xorg\
+  mysql-server 
+
 pip install --upgrade pip
-pip install -r requirements.txt  
+pip install -r "${DEST}"install/requirements.txt  
 
-mysql -u root < create_user.sql 
-mysql -u root < create_tables.sql
+mysql -u root < "${DEST}"install/create_user.sql 
+mysql -u root < "${DEST}"install/create_tables.sql
 
-
-cd /opt/frame/
-for f in /opt/frame/service/*; do 
-  ln -s "$f" /etc/systemd/system/multi-user.target.wants/
+for f in "${DEST}"/service/*; do 
+  ln -sf "$f" /etc/systemd/system/multi-user.target.wants/
 done
 systemctl daemon-reload
 
