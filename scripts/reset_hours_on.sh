ca437204121635982bad1b12650919967043b5a2
agmonr <agmonr@gmail.com>
2019-09-02 13:40:56 +0300

    Update reset_hours_on.sh

--- scripts/reset_hours_on.sh
+++ scripts/reset_hours_on.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# script to set all days to default 
+# the script reset the hours that the frame will show the hour (clock on)
+# sunday-thursday - on 0700, 2000 ,2100
+# friday,  - on 0700, 1300
+# staudrday - on 2000, 2100
+
+
+
 for f in $( seq 1 5); do
   /opt/frame/scripts/update.sh  'clock_on/'$f '{"hours":"000000010000000000001100"}'
 done
