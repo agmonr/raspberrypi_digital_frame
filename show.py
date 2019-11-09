e2a4df946c4ba4dd7d0232aa163b4a4c5eae6d34
ram <agmonr@gmail.com>
2019-11-09 22:24:45 +0200

    wip

--- show.py
+++ show.py
@@ -87,7 +87,8 @@ class show:
     cv2.namedWindow("Frame", cv2.WINDOW_AUTOSIZE )
 #    r = int(int(self.yscreenresulation*1000 / self.img.shape[0]*1000)) # *1000 cause we need better precision
 #    dim = ((self.img.shape[1]*r)/1000000,self.yscreenresulation)
-    self.img=cv2.resize(self.img, (self.xscreenresulation,self.yscreenresulation), interpolation = cv2.INTER_AREA)
+#    self.img=cv2.resize(self.img, (self.xscreenresulation,self.yscreenresulation), interpolation = cv2.INTER_AREA)
+    self.img=self.image_resize(self.img, self.xscreenresulation, self.yscreenresulation)
     cv2.moveWindow("Frame", int((self.xscreenresulation-self.img.shape[1])/2), 0) 
     cv2.imshow("Frame",self.img)
     time.sleep(1)
@@ -99,6 +100,38 @@ class show:
       check=self.check_import_config()
       if check==1:
         break
+
+  def image_resize(self,image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = self.img.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
 
   def update_rest(self,lurl,data):
     etag=requests.get(url+lurl).json()["_etag"]
