cd3a9ba4f616fa38597bc0e12f2ade1ea493a330
Ram <agmonr@gmail.com>
2021-10-26 18:24:47 +0300

    testing on pure pi0

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install vim python3-venv python3-pip htop python3-tk python3-opencv
-python3 -m pip install crontab
+sudo apt install xserver-xorg vim python3-venv python3-pip htop python3-tk python3-opencv
+python3 -m pip install crontab croniter
