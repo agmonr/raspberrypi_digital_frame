cc7e6b1f8d8595e5bb52be437807974b47d8082e
ram <agmonr@gmail.com>
2021-11-11 14:19:15 +0000

    wip

--- install/install.sh
+++ install/install.sh
@@ -4,8 +4,16 @@ sudo apt -y update
 sudo apt-get -y upgrade
 sudo apt install -y vim mv git htop
 sudo apt install -y xserver-xorg python3-venv python3-pip python3-tk 
-python3 -m venv env/ 
+sudo apt install -y  libjasper-dev libjasper-dev python3-pyqt5  libqt4-test libatlas-base-dev 
 
-source env/bin/activate
+
+ln -s /root/raspberrypi_digital_frame/install/frame.service /etc/systemd/system/frame.service
+systemctl daemon-reload
+systemctl enable fra
+
+python3 -m venv ../env/ 
+
+
+source ../env/bin/activate
 ./python_install.sh
 
--- install/python_install.sh
+++ install/python_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo python3 -m pip install --upgrade pip setuptools wheel
-sudo python3 -m pip install -r requirements.txt 
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install -r requirements.txt 
 
